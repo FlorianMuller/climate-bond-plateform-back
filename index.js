d1504bd679699cfde23537ea72f8bbca412c50d6
Florian Muller <fmuller@student.42.fr>
2019-11-16 15:12:39 +0100

    add helmet for security

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(morgan('dev'));
+app.use(helmet());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
